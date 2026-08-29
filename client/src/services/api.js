3ec8b09773e51cb37f0c8fdd38fc929ee25404a8
Vykuntapu Dedeepya <vykuntapudedeepya@gmail.com>
2026-08-29 15:29:45 +0530

    Restore JWT authentication interceptor

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -8,5 +8,16 @@ const api = axios.create({
   baseURL: `${serverUrl}/api`,
 });
 
+// Attach JWT token to every request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export { serverUrl };
 export default api;
